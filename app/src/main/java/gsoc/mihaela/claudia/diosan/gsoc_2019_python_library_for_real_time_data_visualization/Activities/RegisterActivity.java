4424354a71859c99e09ebb269c9b5bac2387c519
claudia <mcd1591@gmail.com>
2019-06-21 16:47:38 +0200

    register activity changed

--- app/src/main/java/gsoc/mihaela/claudia/diosan/gsoc_2019_python_library_for_real_time_data_visualization/Activities/RegisterActivity.java
+++ app/src/main/java/gsoc/mihaela/claudia/diosan/gsoc_2019_python_library_for_real_time_data_visualization/Activities/RegisterActivity.java
@@ -2,14 +2,31 @@ package gsoc.mihaela.claudia.diosan.gsoc_2019_python_library_for_real_time_data_
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckedTextView;
+import android.widget.LinearLayout;
 
 import gsoc.mihaela.claudia.diosan.gsoc_2019_python_library_for_real_time_data_visualization.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private CheckedTextView checkedTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.volunteer_linear_layout);
+        linearLayout.setVisibility(View.GONE);
+        checkedTextView = (CheckedTextView) findViewById(R.id.is_volunteer);
+        checkedTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkedTextView.setChecked(!checkedTextView.isChecked());
+                if (checkedTextView.isChecked()) linearLayout.setVisibility(View.VISIBLE);
+                else linearLayout.setVisibility(View.GONE);
+            }
+        });
     }
 }
